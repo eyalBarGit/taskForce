9dc669e1490b2e32850f7d485ce1cd8ad4ed7578
eyalBarGit <eyalb.media@gmail.com>
2020-12-08 14:44:35 +0200

    Updates

---
+++ precache-manifest.b7e96af8833558605209b59627522292.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df88e291e47d95aa66eacecb47088a4c",
+    "url": "/taskforce/index.html"
+  },
+  {
+    "revision": "0c2e9e72ba43694e376b",
+    "url": "/taskforce/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "c203c83944e1562da98b",
+    "url": "/taskforce/static/css/main.a434c85a.chunk.css"
+  },
+  {
+    "revision": "0c2e9e72ba43694e376b",
+    "url": "/taskforce/static/js/2.c938baa1.chunk.js"
+  },
+  {
+    "revision": "b5ff4111ab81c8c91e9e746653eb6fff",
+    "url": "/taskforce/static/js/2.c938baa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c203c83944e1562da98b",
+    "url": "/taskforce/static/js/main.62537fce.chunk.js"
+  },
+  {
+    "revision": "0f02bb0319bbbd4cd2af",
+    "url": "/taskforce/static/js/runtime-main.95470a51.js"
+  },
+  {
+    "revision": "989f4a8772d2a7e18594d96cae0dd065",
+    "url": "/taskforce/static/media/boraBora.989f4a87.jpg"
+  },
+  {
+    "revision": "c140c866e2b03994d23e4cdce4f531c0",
+    "url": "/taskforce/static/media/lake.c140c866.jpg"
+  },
+  {
+    "revision": "736266ceed0e1c9d5f29d026e853af46",
+    "url": "/taskforce/static/media/mountains.736266ce.jpg"
+  },
+  {
+    "revision": "72bb8802e57df89e6db0605db86fd883",
+    "url": "/taskforce/static/media/purple_nature.72bb8802.jpg"
+  },
+  {
+    "revision": "cc4c0d26de897679f8e928987672164d",
+    "url": "/taskforce/static/media/sky.cc4c0d26.jpg"
+  },
+  {
+    "revision": "d264abfb29f8ce36bd389720b95d1fe6",
+    "url": "/taskforce/static/media/water.d264abfb.jpg"
+  }
+]);
